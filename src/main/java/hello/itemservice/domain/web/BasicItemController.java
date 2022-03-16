20efdeee0f77565a7c4e188559a8aaecccf9ef73
ahakim <aha9654@gmail.com>
2022-03-16 21:22:31 +0900

    수정: Controller - 상품 등록

--- src/main/java/hello/itemservice/domain/web/BasicItemController.java
+++ src/main/java/hello/itemservice/domain/web/BasicItemController.java
@@ -33,12 +33,55 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    //테스트용 데이터
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("새우깡", 1500, 3));
-        itemRepository.save(new Item("감자깡", 1500, 5));
-        itemRepository.save(new Item("옥수수깡", 2000, 5));
-        itemRepository.save(new Item("고구마깡", 2000, 1));
+    /**
+     * MEMO: 상품 등록
+     * 요청 주소(URL)가 같아도 메서드 방식에 따라 기능을 구분할 수 있다.
+     * @GetMapping() : 상품 등록 폼  (뷰 템플릿 반환)
+     * @PostMapping() : 상품 등록 처리(폼 데이터 처리)
+     */
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
     }
-}
+
+    @PostMapping("/add")
+    // MEMO: add-item-v1
+    // public String add(@RequestParam String itemName,
+    //                   @RequestParam int price,
+    //                   @RequestParam Integer quantity,
+    //                   Model model) {
+    //     Item item = new Item(itemName, price, quantity);
+    //     item.setItemName(itemName);
+    //     item.setPrice(price);
+    //     item.setQuantity(quantity);
+    //     itemRepository.save(item);
+    //
+    //     model.addAttribute("item", item);
+    //     return "basic/item";
+    // }
+    // MEMO: add-item-v2 -> @ModelAttribute("값") 추가 시 모델에 자동 추가
+    // @PostMapping("/add")
+    // public String add(@ModelAttribute("item") Item item) {
+    //     itemRepository.save(item);
+    //     return "basic/item";
+    // }
+    // MEMO: [권장] add-item-v3 -> @ModelAttribute() 값 생략
+    public String add(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+    // MEMO: add-item-v4 -> @ModelAttribute 생략
+    // public String add(@ModelAttribute Item item) {
+    //     itemRepository.save(item);
+    //     return "basic/item";
+    // }
+
+        //테스트용 데이터
+        @PostConstruct
+        public void init () {
+            itemRepository.save(new Item("새우깡", 1500, 3));
+            itemRepository.save(new Item("감자깡", 1500, 5));
+            itemRepository.save(new Item("옥수수깡", 2000, 5));
+            itemRepository.save(new Item("고구마깡", 2000, 1));
+        }
+    }
